602ea475efd63a010d893429b05b9ed5d3c8b770
Charlotte McClintock <charlottemcclintock@d-172-26-3-151.dhcp.virginia.edu>
2019-04-22 15:33:13 -0400

    data and first code

---
+++ gen-diff.R
@@ -0,0 +1,52 @@
+
+# Charlotte McClintock
+
+# ...........................................................................................
+
+# load libraries
+library(tidyverse)
+library(plotly)
+
+# ...........................................................................................
+
+df <- read_csv("data.csv")
+
+p1 <- plot_ly(df, color = I("gray80")) %>%
+  add_segments(x = ~mom, xend = ~daughter, y = ~country, yend = ~country, showlegend = FALSE) %>%
+  add_markers(x = ~mom, y = ~country, name = "Mother", color = I("purple")) %>%
+  add_markers(x = ~daughter, y = ~country, name = "Daughter", color = I("pink")) %>%
+  layout(
+    title = "Gender educational disparity",
+    xaxis = list(title = "Mean Years of Education"),
+    margin = list(l = 65)
+  )
+p1
+
+p2 <- plot_ly(df, color = I("gray80")) %>%
+  add_segments(x = ~dad, xend = ~son, y = ~country, yend = ~country, showlegend = FALSE) %>%
+  add_markers(x = ~dad, y = ~country, name = "Father", color = I("navy")) %>%
+  add_markers(x = ~son, y = ~country, name = "Son", color = I("blue")) %>%
+  layout(
+    title = "Gender educational disparity",
+    xaxis = list(title = "Mean Years of Education"),
+    margin = list(l = 65)
+  )
+p2
+
+p <- subplot(p1, p2)
+p
+
+
+p <- plot_ly(df, color = I("gray80")) %>%
+  add_segments(x = ~mom, xend = ~daughter, y = ~country, yend = ~country, showlegend = FALSE) %>%
+  add_markers(x = ~mom, y = ~country, name = "Mother", color = I("purple")) %>%
+  add_markers(x = ~daughter, y = ~country, name = "Daughter", color = I("pink")) %>%
+  add_segments(x = ~dad, xend = ~son, y = ~country, yend = ~country, showlegend = FALSE) %>%
+  add_markers(x = ~dad, y = ~country, name = "Father", color = I("navy")) %>%
+  add_markers(x = ~son, y = ~country, name = "Son", color = I("blue")) %>%
+  layout(
+    title = "Gender educational disparity",
+    xaxis = list(title = "Mean Years of Education"),
+    margin = list(l = 65)
+  )
+p
